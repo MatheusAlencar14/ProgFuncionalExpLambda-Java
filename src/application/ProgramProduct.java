50ff313ad252da3f72c9a72b693c56ae003d500e
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-08-30 10:58:42 -0300

    Implementando Interface Predicate - Versão IV

--- src/application/ProgramProduct.java
+++ src/application/ProgramProduct.java
@@ -6,11 +6,14 @@ import util.ProductPredicate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class ProgramProduct {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("TV", 900.00));
@@ -19,8 +22,12 @@ public class ProgramProduct {
         list.add(new Product("Tablet", 450.00));
         list.add(new Product("Teclado", 69.00));
 
-        //Referência para método: (Nome da Classe::Nome do método)
-        list.removeIf(Product::nonStaticProductPredicate);
+        System.out.print("Digite o valor mínimo: ");
+        int min = sc.nextInt();
+
+        Predicate<Product> prod = p -> p.getPrice() <= min;
+
+        list.removeIf(prod);
 
         for (Product p : list) {
             System.out.println(p);
